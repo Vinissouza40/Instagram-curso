0705ec77eb1fea3bd8a339f3459a02f8fcf6d412
Vinicius Souza <viniciussantos.souza40@gmail.com>
2022-11-06 17:34:44 -0300

    CommitInterfaceCadastro

--- app/src/main/java/com/example/instagram_curso/activity/LoginActivity.java
+++ app/src/main/java/com/example/instagram_curso/activity/LoginActivity.java
@@ -2,15 +2,29 @@ package com.example.instagram_curso.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.instagram_curso.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private EditText editEmailLogin, editSenhaLogin;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+    }
+
+    public void abrirCadastro(View view){
+
+        Intent i = new Intent(LoginActivity.this, CadastroActivity.class);
+        startActivity(i);
     }
 }
