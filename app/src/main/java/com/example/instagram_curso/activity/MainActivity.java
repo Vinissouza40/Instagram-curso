3873ab732affb315db39f98e9f3a395b8f72135c
Vinicius Souza <viniciussantos.souza40@gmail.com>
2022-11-08 15:41:49 -0300

    CommitTelaPerfil

--- app/src/main/java/com/example/instagram_curso/activity/MainActivity.java
+++ app/src/main/java/com/example/instagram_curso/activity/MainActivity.java
@@ -3,16 +3,25 @@ package com.example.instagram_curso.activity;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.FrameLayout;
 
 import com.example.instagram_curso.R;
 import com.example.instagram_curso.config.ConfiguracaoFirebase;
+import com.example.instagram_curso.fragment.FeedFragment;
+import com.example.instagram_curso.fragment.PerfilFragment;
+import com.example.instagram_curso.fragment.PesquisaFragment;
+import com.example.instagram_curso.fragment.PostagemFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,8 +36,68 @@ public class MainActivity extends AppCompatActivity {
         toolbar.setTitle("Instagram");
         setSupportActionBar(toolbar);
 
+
+
+
         autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
 
+
+        configuraBottomVavigationView();
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        fragmentTransaction.replace(R.id.viewPager, new FeedFragment()).commit();
+
+
+    }
+
+    public void configuraBottomVavigationView(){
+
+        BottomNavigationViewEx bottomNavigationViewEx =  findViewById(R.id.bottomNavigation);
+
+        habilitarNavegacao(bottomNavigationViewEx);
+
+        Menu menu = bottomNavigationViewEx.getMenu();
+        MenuItem menuItem = menu.getItem(0);
+        menuItem.setChecked(true);
+
+
+    }
+
+    /**
+     * metodo respnsavel por tratar evenros de click na BottomNavigation
+     * @param viewEx
+     */
+
+    private void habilitarNavegacao(BottomNavigationViewEx viewEx){
+        viewEx.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+                switch (item.getItemId()){
+                    case R.id.ic_home:
+                        fragmentTransaction.replace(R.id.viewPager, new FeedFragment()).commit();
+
+                        return true;
+                    case R.id.ic_pesquisa:
+                        fragmentTransaction.replace(R.id.viewPager, new PesquisaFragment()).commit();
+                        return true;
+
+                        case R.id.ic_postagem:
+                        fragmentTransaction.replace(R.id.viewPager, new PostagemFragment()).commit();
+                        return true;
+
+                    case R.id.ic_perfil:
+                        fragmentTransaction.replace(R.id.viewPager, new PerfilFragment()).commit();
+                        return true;
+                }
+
+                return false;
+            }
+        });
     }
 
     @Override
