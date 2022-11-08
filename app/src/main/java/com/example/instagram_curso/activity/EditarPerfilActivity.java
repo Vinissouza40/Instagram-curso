48ee2598d655a75fddce8a83d4f2347cb0af87eb
Vinicius Souza <viniciussantos.souza40@gmail.com>
2022-11-08 16:55:03 -0300

    CommitTelaEdicaoPerfil

---
+++ app/src/main/java/com/example/instagram_curso/activity/EditarPerfilActivity.java
@@ -0,0 +1,16 @@
+package com.example.instagram_curso.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.instagram_curso.R;
+
+public class EditarPerfilActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editar_perfil);
+    }
+}
--- app/src/main/java/com/example/instagram_curso/fragment/PerfilFragment.java
+++ app/src/main/java/com/example/instagram_curso/fragment/PerfilFragment.java
@@ -1,5 +1,6 @@
 package com.example.instagram_curso.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,12 +8,24 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.GridView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.example.instagram_curso.R;
+import com.example.instagram_curso.activity.EditarPerfilActivity;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 
 public class PerfilFragment extends Fragment {
 
+    private ProgressBar progressBar;
+    private CircleImageView imagePerfil;
+    public GridView gridViewPerfil;
+    private TextView textPublicacoes, textSeguidores, textSeguindo;
+    private Button buttonEditarPerfil;
 
     public PerfilFragment() {
 
@@ -29,6 +42,24 @@ public class PerfilFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_perfil, container, false);
+        View view = inflater.inflate(R.layout.fragment_perfil, container, false);
+
+        gridViewPerfil = view.findViewById(R.id.grifViewPerfil);
+        progressBar = view.findViewById(R.id.progressBarPerfil);
+        imagePerfil = view.findViewById(R.id.imagePerfil);
+        textPublicacoes = view.findViewById(R.id.textPublicacoes);
+        textSeguidores = view.findViewById(R.id.textSeguidores);
+        textSeguindo = view.findViewById(R.id.textSeguindo);
+        buttonEditarPerfil = view.findViewById(R.id.buttonEditarPerfil);
+
+        buttonEditarPerfil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getActivity(), EditarPerfilActivity.class);
+                startActivity(i);
+            }
+        });
+
+        return view;
     }
 }
