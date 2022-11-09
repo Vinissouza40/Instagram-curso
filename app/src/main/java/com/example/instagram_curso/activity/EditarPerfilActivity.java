70c97029d2068af8b9a8775b4dc26a1b86c4b954
Vinicius Souza <viniciussantos.souza40@gmail.com>
2022-11-09 08:58:52 -0300

    CommitTelaEditarPerfil

--- app/src/main/java/com/example/instagram_curso/activity/EditarPerfilActivity.java
+++ app/src/main/java/com/example/instagram_curso/activity/EditarPerfilActivity.java
@@ -1,6 +1,7 @@
 package com.example.instagram_curso.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -12,5 +13,12 @@ public class EditarPerfilActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editar_perfil);
+
+        Toolbar toolbar = findViewById(R.id.toolbarPerfil);
+        toolbar.setTitle("Editar perfil");
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_baseline_close_24);
     }
 }
