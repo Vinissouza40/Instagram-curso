d8057a64f722177eef53b520f4d2142654ffdcc2
Vinicius Souza <viniciussantos.souza40@gmail.com>
2022-11-10 11:07:44 -0300

    CommitModificacoesTelaPesquisaUsuario

--- app/src/main/java/com/example/instagram_curso/fragment/PesquisaFragment.java
+++ app/src/main/java/com/example/instagram_curso/fragment/PesquisaFragment.java
@@ -2,6 +2,7 @@ package com.example.instagram_curso.fragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -12,11 +13,23 @@ import android.view.ViewGroup;
 import android.widget.SearchView;
 
 import com.example.instagram_curso.R;
+import com.example.instagram_curso.config.ConfiguracaoFirebase;
+import com.example.instagram_curso.model.Usuario;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class PesquisaFragment extends Fragment {
 
     private SearchView searchViewPesquisa;
     private RecyclerView recyclerPesquisa;
+    private List<Usuario> listaUsuarios;
+    private DatabaseReference usuariosRef;
 
     public PesquisaFragment() {
 
@@ -38,21 +51,52 @@ public class PesquisaFragment extends Fragment {
         searchViewPesquisa = view.findViewById(R.id.searchViewPesquisa);
         recyclerPesquisa = view.findViewById(R.id.recyclerPesquisa);
 
+        listaUsuarios = new ArrayList<>();
+        usuariosRef = ConfiguracaoFirebase.getFirebaseDatabase().child("usuarios");
+
         searchViewPesquisa.setQueryHint("Buscar Usuários");
         searchViewPesquisa.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String s) {
-
                 return false;
             }
 
             @Override
             public boolean onQueryTextChange(String newText) {
-
+                String textoDigitado = newText.toUpperCase();
+                pesquisarUsuarios(textoDigitado);
                 return true;
             }
         });
 
         return view;
+    }
+
+    private void pesquisarUsuarios(String texto) {
+
+        listaUsuarios.clear();
+
+        if (texto.length() > 0){
+            Query query = usuariosRef.orderByChild("nome").startAt(texto).endAt(texto + "\uf8ff");
+            query.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    for (DataSnapshot ds : snapshot.getChildren()){
+                        listaUsuarios.add(ds.getValue(Usuario.class));
+                    }
+
+                    int total = listaUsuarios.size();
+                    Log.i("totalUsuarios", "total: " + total);
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+
+                }
+            });
+
+        }
+
+
     }
 }
